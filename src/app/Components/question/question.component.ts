adc270e657da9c667af685f3ed6ee94d3ea18f7a
AdaptyIndia <umesh.chaurasiya@adapty.com>
2017-11-02 15:43:15 +0530

    collapsable added

--- src/app/Components/question/question.component.ts
+++ src/app/Components/question/question.component.ts
@@ -13,25 +13,55 @@ export class QuestionComponent implements OnInit {
 
   constructor( private _questionService: QuestionService ) { }
   questions = [];
+  globalQuestions = [];
+  visibleQuestions = [];
 
-  	// Q2 = false;
-   // 	Q3 = false;
-   // 	Q1 = false;
-   // 	Q4 = false;
-   // 	Q5 = false;
-   // 	Q6 = false;
-   // 	Q7 = false;
-   // 	Q8 = false;
-   // 	Q9 = false;
-   // 	Q10 = false;
-   // 	Q11 = false;
-   // 	Q12 = false;
-   // 	Q13 = false;
+  private _prevSelected: any;
+
+    collapsed = false;
+    toggleCollapse(evt){
+
+      this.collapsed = !this.collapsed;
+    }
+    loadDependentQuestions(evt) {
+      let target = evt.target;
+      //console.log('handleChange ',target.id.split('_'));
+      let question_id,answer_id;
+      if (target.nodeName.toLowerCase() == "input") {
+          question_id = target.id.split('_')[1];
+          answer_id = target.id.split('_')[2];;
+      }else if( target.nodeName.toLowerCase() == "select" ){
+          question_id = target.id.split('_')[1];
+          answer_id = target.value;;
+      }
+      if (question_id && answer_id) {
+        this._questionService.getProgram(question_id,answer_id).subscribe(programs => {
+          if(programs && programs.length > 0 ){
+            this._questionService.getQuestionsByProgramId(programs[0]).subscribe(questions => {
+            //  console.log('questions :',questions);
+              this.questions = this.globalQuestions.concat(questions);
+            });
+          }
+        });
+      };
+      
+      
+    }
+
 
   ngOnInit() {
 
-  	this._questionService.getAllQuestions()
-  	.subscribe(resQuestionData => this.questions = resQuestionData);
+  	// this._questionService.getAllQuestions()
+  	// .subscribe(resQuestionData => this.questions = resQuestionData);
+
+    this._questionService.getQuestionByScope('global')
+    .subscribe(resQuestionData => {
+      this.globalQuestions = resQuestionData;
+      this.questions = this.globalQuestions;
+    });
+    
+
+
   }
 
 }
--- src/app/Services/question.service.ts
+++ src/app/Services/question.service.ts
@@ -6,6 +6,10 @@ import {
     Response
 } from '@angular/http';
 import 'rxjs/add/operator/map';
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/operator/filter";
+
+
 
 
 @Injectable()
@@ -29,6 +33,47 @@ export class QuestionService {
 
     getQuestionsByProgramId( _pid: string ){
         if (!_pid) {return null;};
-        return [];
+         return this._http.get(this._url).map((response: Response) => {
+            let _response = response.json();
+            //console.log('_response : ',_response.configuration.programs[0]['questions']);
+            return _response.questions.filter(function(question){
+                return question.program && question.program.indexOf(_pid) >= 0  ;
+            });
+        });
+    }
+
+    getQuestionsMultipleProgramIds( _ids: Array<string> ){
+        if (!_ids || _ids.length == 0) {return null;};
+        let questions = [];
+        
+        return questions;
+    }
+
+    getQuestionByScope( _scope: string ){
+        if (!_scope) {return null;};
+        return this._http.get(this._url).map((response: Response) => {
+            let _response = response.json();
+            //console.log('_response : ',_response.configuration.programs[0]['questions']);
+            return _response.questions.filter(function(question){
+                return question.scope && question.scope == _scope;
+            });
+        });
+    }
+
+    getProgram( _questionid: string, optionid: string){
+        if (!_questionid || !optionid ) {return null;};
+        return this._http.get(this._url).map((response: Response) => {
+            let _response = response.json();
+            //console.log('_response : ',_response.configuration.programs[0]['questions']);
+            let question = _response.questions.filter(function(q){
+                return q.q_id == _questionid;
+            });
+
+            let option = question[0].q_options.filter(function(opt){
+                return opt.opt_value == optionid;
+            });
+            //console.log('option.program : ',option[0].program);
+            return option[0].program;
+        });
     }
 }
